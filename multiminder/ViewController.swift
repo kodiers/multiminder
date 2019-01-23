341b22d09a4d6cebd4eef9ab59f147526c15e594
Victor Yamchinov <kodiers@gmail.com>
2019-01-23 05:33:25 +0300

    Initial Commit

---
+++ multiminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  multiminder
+//
+//  Created by Viktor Yamchinov on 23/01/2019.
+//  Copyright © 2019 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
